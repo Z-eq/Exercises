a2de1887e25578bd1b9a74a350d93a05a6bb105a
Zeq Alidemaj <zeq.alidemaj@gmail.com>
2022-06-01 22:38:29 +0200

    avarege numbers

---
+++ src/main/java/org/zeq/AvarageNums.java
@@ -0,0 +1,13 @@
+package org.zeq;
+
+public class AvarageNums {
+
+        public static void main(String[] args) {
+            int avg;
+            int a=23, b=11, c=77;
+            avg=(a+b+c)/3;
+            //Jag är osäker på om det finns enklare sätt att skriva sout utan att ange värden?
+            System.out.println("(23+11+77)/3)="+avg);
+        }
+    }
+
