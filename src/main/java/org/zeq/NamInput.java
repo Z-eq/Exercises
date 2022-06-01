13811a377160fc1359d1140367419efd6e0017fb
Zeq Alidemaj <zeq.alidemaj@gmail.com>
2022-06-01 22:55:35 +0200

    Hello name Input

--- src/main/java/org/zeq/NamInput.java
+++ src/main/java/org/zeq/NamInput.java
@@ -7,7 +7,7 @@ public class NamInput {
         public static void main(String[] args)
         {
             Scanner input = new Scanner (System.in);
-            System.out.print("Input your first name: ");
+            System.out.print("Enter your name: ");
             String name = input.next();
             System.out.println("Hello "+name+"!");
         }
