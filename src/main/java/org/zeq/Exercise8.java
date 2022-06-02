929ccaf95429512129c6508812738beaf4b7d87c
Zeq Alidemaj <zeq.alidemaj@gmail.com>
2022-06-02 23:25:13 +0200

    Exercise 8 guessing game!

--- src/main/java/org/zeq/Exercise8.java
+++ src/main/java/org/zeq/Exercise8.java
@@ -12,63 +12,45 @@ public class Exercise8 {
             Scanner sc = new Scanner(System.in);
 
             // Generate the numbers
-            int number = 1 + (int)(500
-                    * Math.random());
+            int number = 1 + (int)(51 * Math.random());
 
             // Given K trials
-            int K = 501;
+            int K = 51;
 
             int i, guess;
 
-            System.out.println(
-                    "A number is chosen"
-                            + " between 1 to 500."
-                            + "Guess the number"
-                            + " within 10 trials.");
+            System.out.println( "A number is chosen" + " between 1 to 500."+ "Guess the number"+ " within 50 trials.");
 
             // Iterate over K Trials
             for (i = 0; i < K; i++) {
 
-                System.out.println(
-                        "Guess the number:");
+                System.out.println( "Guess the number:");
 
                 // Take input for guessing
                 guess = sc.nextInt();
 
                 // If the number is guessed
                 if (number == guess) {
-                    System.out.println(
-                            "Congratulations!"
-                                    + " You guessed the number.");
+                    System.out.println("Congratulations!" + " You guessed the number.");
                     break;
                 }
-                else if (number > guess
-                        && i != K - 1) {
-                    System.out.println(
-                            "The number is "
-                                    + "greater than " + guess);
+                else if (number > guess && i != K - 1) {
+                    System.out.println("The number is " + "greater than " + guess);
                 }
-                else if (number < guess
-                        && i != K - 1) {
-                    System.out.println(
-                            "The number is"
-                                    + " less than " + guess);
+                else if (number < guess && i != K - 1) {
+                    System.out.println("The number is" + " less than " + guess);
                 }
             }
 
             if (i == K) {
-                System.out.println(
-                        "You have exhausted"
-                                + " K trials.");
+                System.out.println( "You have exhausted" + " K trials.");
 
-                System.out.println(
-                        "The number was " + number);
+                System.out.println( "The number was " + number);
             }
         }
 
         // Driver Code
-        public static void
-        main(String arg[])
+     public static void main(String[] arg)
         {
 
             // Function Call
