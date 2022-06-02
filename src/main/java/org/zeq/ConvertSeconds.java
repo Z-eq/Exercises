a7064cd7fe6807c3631b0b5e6b41614631ef0774
Zeq Alidemaj <zeq.alidemaj@gmail.com>
2022-06-02 08:24:13 +0200

    Convert Seconds to minutes/houers/seconds

--- src/main/java/org/zeq/ConvertSeconds.java
+++ src/main/java/org/zeq/ConvertSeconds.java
@@ -3,6 +3,15 @@ package org.zeq;
 public class ConvertSeconds {
     public static void main(String[] args) {
 
-        // TODO latest on Friday!
-    }
-}
+                int sec = 86399;
+                int x = sec % 60;
+                int y = sec / 60;
+                int z = y % 60;
+                y = y / 60;
+                System.out.print( y + ":" + z + ":" + x);
+
+
+
+            }
+        }
+
